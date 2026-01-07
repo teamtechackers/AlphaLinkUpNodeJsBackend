bf8ae854313444a86164c4c5931bd1730d2e498f
ghulamshahbazali <shahbazamanat93@gmail.com>
2026-01-07 16:42:34 +0500

    Add debug environment route

--- src/routes/index.js
+++ src/routes/index.js
@@ -26,6 +26,7 @@ const JobController = require('../controllers/JobController');
 const FolderController = require('../controllers/FolderController');
 const FCMTokenController = require('../controllers/FCMTokenController');
 const UserController = require('../controllers/UserController');
+const debugRoutes = require('./debug');
 
 const authenticate = require('../middlewares/authenticate');
 const adminAuth = require('../middlewares/adminAuth');
@@ -49,6 +50,7 @@ router.get('/health', (req, res) => {
   });
 });
 
+router.use('/debug', debugRoutes);
 // API version endpoint
 router.get('/version', (req, res) => {
   res.status(200).json({
